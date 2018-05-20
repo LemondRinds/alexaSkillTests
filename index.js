575765f2d0dd0c76689cb61083e6ef6b12e4163a
Erik Pyell <erik.pyell@superion.com>
2018-05-19 21:23:09 -0400

    bare bonezd the dependencies manually, dont bother letting npm install the aws sdk package or its dependencies, causes bloat. tinyreq added for promise use

--- index.js
+++ index.js
@@ -1,60 +1,16 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
-
 'use strict';
 const Alexa = require('alexa-sdk');
-const request = require('request');
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-import { APP_ID } from './appId.js';
-
-const SKILL_NAME = 'Space Facts';
-const GET_FACT_MESSAGE = "Here's your fact: ";
-const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
-const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'Goodbye!';
-
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
-//=========================================================================================================================================
-const data = [
-    'A year on Mercury is just 88 days long.',
-    'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-    'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-    'On Mars, the Sun appears about half the size as it does on Earth.',
-    'Earth is the only planet not named after a god.',
-    'Jupiter has the shortest day of all the planets.',
-    'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-    'The Sun contains 99.86% of the mass in the Solar System.',
-    'The Sun is an almost perfect sphere.',
-    'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-    'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-    'The temperature inside the Sun can reach 15 million degrees Celsius.',
-    'The Moon is moving approximately 3.8 cm away from our planet every year.',
-];
-
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
-
+const tinyreq = require("tinyreq");
+const AppId = require('appId');
+const SKILL_NAME = 'bobsuruncle';
 const handlers = {
-    'LaunchRequest': () => {
+    'LaunchRequest': function(){
         this.response.speak('Test Launch Request received.');
         //this.response.shouldEndSession(true);
-        this.emitWithState(':responseReady');
+        this.emit(':responseReady');
     },
-    'questionIntent': () => {
+    'questionIntent': function(){
+		console.log('started intent')
         const theNumber = this.event.request.intent.slots.number.value;
         var query = parseInt(theNumber);
         if(theNumber == undefined || isNaN(query)){
@@ -62,48 +18,79 @@ const handlers = {
         }
         this.response.speak(query);
         if(query == 1){
-            this.emit('HelloWorldA');
+            HelloWorldA(this);
         }
         if(query == 2){
-            this.emit('HelloWorldB');
+            HelloWorldB(this); 
         }
-        
-        const url = `http://temp.cloudwatch.net/wp-json/wp/v2/users/me`;
-        request.get(url, (error, response, body) => {
-            let json = JSON.parse(body);
-            console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', json); // Print the body
-            this.response.speak(json.message + " gotten from wordpress json").listen("Do another?");
+        var url = 'http://temp.cloudwatch.net/wp-json/wp/v2/users/me'
+        tinyreq({url:url}).then(body => {
+            var json = JSON.parse(body);
+            this.response.speak('then ' + json.message);
+            this.emit(':responseReady');
+        }).catch(err => {
+            var json = JSON.parse(err);
+            var err = json.data.status
+            if(err == undefined){ err = 'Bad error' };
+            this.response.speak('catch ' + err);
+            this.emit(':responseReady');
         });
-		this.emit(':responseReady');
-    },
-    'HelloWorldA': () => {
-      this.response.speak('Did a.');
-    },
-    'HelloWorldB': () => {
-      this.response.speak('Did b.'); 
-    },
-    'AMAZON.HelpIntent': () => {
-        const speechOutput = HELP_MESSAGE;
-        const reprompt = HELP_REPROMPT;
-
+},
+    'AMAZON.HelpIntent': function(){
+        const speechOutput = "Help.";
+        const reprompt = "I mean, no.";
         this.response.speak(speechOutput).listen(reprompt);
         this.emit(':responseReady');
     },
-    'AMAZON.CancelIntent': () => {
-        this.response.speak(STOP_MESSAGE);
+    'AMAZON.CancelIntent': function(){
+        this.response.speak("Canceled");
         this.emit(':responseReady');
     },
-    'AMAZON.StopIntent': () => {
-        this.response.speak(STOP_MESSAGE);
+    'AMAZON.StopIntent': function(){
+        this.response.speak("Stopped");
         this.emit(':responseReady');
-    },
+    }
 };
-
-exports.handler = (event, context, callback) => {
+exports.handler = function(event, context, callback){
     const alexa = Alexa.handler(event, context, callback);
-    alexa.APP_ID = APP_ID;
+    alexa.APP_ID = AppId.APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
+function HelloWorldA(ths){
+    ths.response.speak('Did a.');
+}
+function HelloWorldB(ths){
+    ths.response.speak('Did b.');
+}
+/*function repGet(ths){
+    reqOutHandle(ths, 'http://temp.cloudwatch.net/wp-json/wp/v2/users/me');
+}
+function reqGet(ths, url){
+    reqOutHandle(ths, url, {}, {});
+}
+function reqGet(ths, url, headers){
+    reqOutHandle(ths, url, headers, {});
+}
+function reqPost(ths, url, headers){
+    reqOutHandle(ths, url, headers);
+}
+function reqPost(ths, url, headers, data){
+    reqOutHandle(ths, url, headers, data);
+}
+function reqOutHandle(ths, url, headers, data){
+    var opts = {url:url};
+    if(headers.length > 0){
+        opts.headers = headers;
+    }
+    if(data.length > 0){
+        opts.data = data;
+    }
+    tinyreq(opts).then(body => {
+       ths.response.speak('then');
+       ths.emit(':responseReady');
+    }).catch(err => {
+       ths.response.speak('catch');
+       ths.emit(':responseReady');
+    });
+}*/
